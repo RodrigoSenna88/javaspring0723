c4285af2f7414d228cadb7323f2fee99c0866b59
Rodrigo <rodrigosennasantos@hotmail.com>
2023-07-11 18:39:35 -0300

    criando projeto

---
+++ src/main/java/com/rodrigosantos/javaspring0723/Javaspring0723Application.java
@@ -0,0 +1,13 @@
+package com.rodrigosantos.javaspring0723;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaspring0723Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaspring0723Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigosantos/javaspring0723/Javaspring0723ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigosantos.javaspring0723;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaspring0723ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
